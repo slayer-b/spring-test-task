4f4bf618cc00d53b51c7d1cb94e023b57560f628
dmitrodemchuk <y804668@syniverse.com>
2025-01-21 11:42:06 +0200

    added mapstruct for mappers

--- src/main/java/com/my/mapper/BlogMapper.java
+++ src/main/java/com/my/mapper/BlogMapper.java
@@ -2,10 +2,13 @@ package com.my.mapper;
 
 import com.my.domain.Blog;
 import com.my.dto.BlogDTO;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface BlogMapper {
 
     BlogDTO makeBlogDTO(Blog blog);
 
     Blog makeBlogEntity(BlogDTO blogDTO);
+
 }
